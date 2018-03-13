d11aad9c390744dc05857ab0b51bd9989cffa398
Mingun <alexander_sergey@mail.ru>
2018-03-14 01:06:15 +0500

    Implementation

---
+++ .eslintrc.js
@@ -0,0 +1,164 @@
+"use strict";
+
+module.exports = {
+  extends: "eslint:recommended",
+  root: true,
+  env: { node: true },
+  parserOptions: { ecmaVersion: 2015 },
+
+  rules: {
+    // ----- Possible Errors -----
+    "no-console": "off",
+    "no-control-regex": "off",
+    "no-irregular-whitespace": ["error", {
+      skipStrings: false,
+      skipComments: false,
+      skipRegExps: false,
+      skipTemplates: false
+    }],
+    "no-prototype-builtins": "error",
+    "no-regex-spaces": "off",
+    "valid-jsdoc": "error",
+
+    // ----- Best Practices -----
+    "consistent-return": "error",
+    "curly": "error",
+    "default-case": "error",
+    "dot-location": ["error", "property"],
+    "dot-notation": "error",
+    "eqeqeq": "error",
+    "guard-for-in": "error",
+    "no-caller": "error",
+    "no-eq-null": "error",
+    "no-extend-native": "error",
+    "no-extra-bind": "error",
+    "no-extra-label": "error",
+    "no-floating-decimal": "error",
+    "no-implicit-coercion": "error",
+    "no-implicit-globals": "error",
+    "no-implied-eval": "error",
+    "no-iterator": "error",
+    "no-lone-blocks": "error",
+    "no-loop-func": "error",
+    "no-multi-str": "error",
+    "no-new": "error",
+    "no-new-func": "error",
+    "no-new-wrappers": "error",
+    "no-octal-escape": "error",
+    "no-proto": "error",
+    "no-return-assign": "error",
+    "no-self-compare": "error",
+    "no-sequences": "error",
+    "no-throw-literal": "error",
+    "no-unmodified-loop-condition": "error",
+    "no-unused-expressions": "error",
+    "no-useless-call": "error",
+    "no-useless-concat": "error",
+    "no-void": "error",
+    "no-warning-comments": "error",
+    "no-with": "error",
+    "radix": "error",
+    "wrap-iife": ["error", "inside"],
+    "yoda": "error",
+
+    // ----- Strict Mode -----
+    "strict": "error",
+
+    // ----- Variables -----
+    "no-label-var": "error",
+    "no-shadow-restricted-names": "error",
+    "no-undef-init": "error",
+    "no-buffer-constructor": "error",
+
+    // ----- Node.js and CommonJS -----
+    "no-new-require": "error",
+
+    // ----- Stylistic Issues -----
+    "array-bracket-spacing": ["error", "never"],
+    "block-spacing": ["error", "always"],
+    "brace-style": ["error", "1tbs", { allowSingleLine: true }],
+    "camelcase": ["error", { properties: "never" }],
+    "comma-dangle": ["error", "only-multiline"],
+    "comma-spacing": ["error", { before: false, after: true }],
+    "computed-property-spacing": ["error", "never"],
+    "consistent-this": ["error", "that"],
+    "eol-last": ["error", "always"],
+    "func-call-spacing": ["error", "never"],
+    "func-style": ["error", "declaration"],
+    "indent-legacy": ["error", 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      MemberExpression: 1,
+      FunctionDeclaration: { parameters: 1, body: 1 },
+      FunctionExpression: { parameters: 1, body: 1 }
+    }],
+    "key-spacing": ["error", {
+      beforeColon: false,
+      afterColon: true,
+      mode: "minimum"
+    }],
+    "keyword-spacing": ["error", { before: true, after: true }],
+    "padding-line-between-statements": ["error",
+      { blankLine: "always", prev: "directive", next: "*" },
+      { blankLine: "any", prev: "directive", next: "directive" },
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "any", prev: ["var", "let", "const", "block-like"], next: "return" },
+    ],
+    "max-len": ["error", {
+      code: 100,
+      ignoreComments: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true
+    }],
+    "new-cap": ["error", { newIsCap: true, capIsNew: true, properties: true }],
+    "new-parens": "error",
+    "no-array-constructor": "error",
+    "no-multiple-empty-lines": ["error", { max: 1 }],
+    "no-new-object": "error",
+    "no-tabs": "error",
+    "no-trailing-spaces": "error",
+    "no-unneeded-ternary": "error",
+    "no-whitespace-before-property": "error",
+    "object-curly-spacing": ["error", "always"],
+    "one-var": ["error", { initialized: "never", uninitialized: "always" }],
+    "operator-assignment": ["error", "always"],
+    "operator-linebreak": ["error", "before"],
+    "padded-blocks": ["error", "never"],
+    "quote-props": ["error", "consistent"],
+    "quotes": ["error", "single", { "avoidEscape": true }],
+    "semi": ["error", "always"],
+    "semi-spacing": ["error", { before: false, after: true }],
+    "space-before-blocks": ["error", "always"],
+    "space-before-function-paren": ["error", "never"],
+    "space-infix-ops": ["error", { "int32Hint": true }],
+    "space-unary-ops": ["error", { words: true, nonwords: false }],
+    "spaced-comment": ["error", "always", {
+      line: { markers: ["/", "{", "}"] },
+      block: { markers: ["*"], balanced: true }
+    }],
+    "unicode-bom": ["error", "never"],
+
+    // ----- ECMAScript 6 -----
+    "arrow-body-style": ["error", "as-needed"],
+    "arrow-parens": ["error", "as-needed"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "generator-star-spacing": ["error", "after"],
+    "no-duplicate-imports": ["error", { includeExports: true }],
+    "no-useless-computed-key": "error",
+    "no-useless-constructor": "error",
+    "no-useless-rename": "error",
+    "no-var": "error",
+    "object-shorthand": ["error", "methods"],
+    "prefer-numeric-literals": "error",
+    "prefer-rest-params": "error",
+    "prefer-spread": "error",
+    "rest-spread-spacing": ["error", "never"],
+    "sort-imports": ["error", {
+      memberSyntaxSortOrder: ["none", "all", "multiple", "single"]
+    }],
+    "symbol-description": "error",
+    "template-curly-spacing": ["error", "never"],
+    "yield-star-spacing": ["error", "after"],
+  }
+}
---
+++ test/diff.js
@@ -0,0 +1,191 @@
+'use strict';
+
+let diff   = require('../lib/index.js');
+let schema = require('../diff.schema.json');
+
+let chai = require('chai');
+let expect = chai.expect;
+
+chai.use(require('chai-json-schema'));
+chai.use(function() {
+  chai.Assertion.addMethod('diff', function(diff) {
+    return new chai.Assertion(this._obj)
+      .to.be.jsonSchema(schema)
+      .and.deep.equals(diff);
+  });
+});
+
+function simpleClone(obj) {
+  if (obj !== undefined) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+  return undefined;
+}
+
+describe('json-text-diff', function() {
+  it('returns value corresponding to JSON schema', function() {
+    expect(diff('a', 'b')).to.be.an.jsonSchema(schema);
+    expect(diff(true, false)).to.be.an.jsonSchema(schema);
+  });
+
+  describe('returns empty array for the same objects', function() {
+    const SAME_OBJECTS = [
+      { name: 'undefined',
+        value: undefined,
+        diff: [{ kind: ' ', value: 'undefined' }]
+      },
+      { name: 'null',
+        value: null,
+        diff: [{ kind: ' ', value: 'null' }]
+      },
+      { name: 'true',
+        value: true,
+        diff: [{ kind: ' ', value: 'true' }]
+      },
+      { name: 'false',
+        value: false,
+        diff: [{ kind: ' ', value: 'false' }]
+      },
+      { name: 'number',
+        value: 123.456,
+        diff: [{ kind: ' ', value: '123.456' }]
+      },
+      { name: 'string',
+        value: 'test subject',
+        diff: [{ kind: ' ', value: 'test subject' }]
+      },
+      { name: 'empty array',
+        value: [],
+        diff: [{ kind: ' ', value: '[]' }]
+      },
+      { name: 'non-empty array',
+        value: ['a', 'bcdef', 42, true],
+        diff: [
+          { kind: ' ', value: '['         },
+          { kind: ' ', value: '  "a"'     },
+          { kind: ' ', value: '  "bcdef"' },
+          { kind: ' ', value: '  42'      },
+          { kind: ' ', value: '  true'    },
+          { kind: ' ', value: ']'         },
+        ]
+      },
+      { name: 'empty object',
+        value: {},
+        diff: [{ kind: ' ', value: '{}' }]
+      },
+      { name: 'non-empty object',
+        value: { key: 'value', 42: 'answer' },
+        diff: [
+          { kind: ' ', value: '{'                },
+          { kind: ' ', value: '  "42": "answer"' },
+          { kind: ' ', value: '  "key": "value"' },
+          { kind: ' ', value: '}'                },
+        ]
+      },
+    ];
+    for (let desc of SAME_OBJECTS) {
+      it(desc.name, function() {
+        expect(diff(desc.value, simpleClone(desc.value))).to.be.diff(desc.diff);
+      });
+    }
+  });
+
+  it('handle arrays with circular referencies', function() {
+    function makeDiff(invert) {
+      let kind1 = invert ? '+' : '-';
+      let kind2 = invert ? '-' : '+';
+      let part1 = [
+        { kind: kind1, value: '[' },
+        { kind: kind1,
+          changes: [
+            { kind: kind1, value: '  '         },
+            { kind: ' ',   value: '"'          },
+            { kind: kind1, value: '[Circular]' },
+            { kind: ' ',   value: '"'          },
+          ]
+        },
+        { kind: kind1, value: ']' },
+      ];
+      let part2 = [
+        { kind: kind2,
+          changes: [
+            { kind: ' ',   value: '"'                  },
+            { kind: kind2, value: 'some boring string' },
+            { kind: ' ',   value: '"'                  },
+          ]
+        }
+      ];
+
+      return invert ? part2.concat(part1) : part1.concat(part2);
+    }
+
+    let obj = [];
+    obj.push(obj);
+
+    expect(diff(obj, 'some boring string')).to.be.diff(makeDiff(false));
+    expect(diff('some boring string', obj)).to.be.diff(makeDiff(true ));
+  });
+
+  it('handle objects with circular referencies', function() {
+    function makeDiff(invert) {
+      let kind1 = invert ? '+' : '-';
+      let kind2 = invert ? '-' : '+';
+      let part1 = [
+        { kind: kind1, value: '{' },
+        { kind: kind1,
+          changes: [
+            { kind: kind1, value: '  '          },
+            { kind: ' ',   value: '"'           },
+            { kind: kind1, value: 'self":'      },
+            { kind: ' ',   value: ' '           },
+            { kind: ' ',   value: '"'           },
+            { kind: kind1, value: '[Circular]"' },
+          ]
+        },
+        { kind: kind1, value: '}' },
+      ];
+      let part2 = [
+        { kind: kind2,
+          changes: [
+            { kind: ' ',   value: '"'             },
+            { kind: kind2, value: 'some'          },
+            { kind: ' ',   value: ' '             },
+            { kind: kind2, value: 'boring string' },
+            { kind: ' ',   value: '"'             },
+          ]
+        }
+      ];
+
+      return invert ? part2.concat(part1) : part1.concat(part2);
+    }
+    let obj = {};
+    obj.self = obj;
+
+    expect(diff(obj, 'some boring string')).to.be.diff(makeDiff(false));
+    expect(diff('some boring string', obj)).to.be.diff(makeDiff(true ));
+  });
+
+  it('generates inline differencies', function() {
+    expect(diff(
+      'some foo string',
+      'some bar string'
+    )).to.deep.equal([
+      {
+        kind: '-',
+        changes: [
+          { kind: ' ', value: 'some '   },
+          { kind: '-', value: 'foo'     },
+          { kind: ' ', value: ' string' },
+        ]
+      },
+      {
+        kind: '+',
+        changes: [
+          { kind: ' ', value: 'some '   },
+          { kind: '+', value: 'bar'     },
+          { kind: ' ', value: ' string' },
+        ]
+      },
+    ]);
+  });
+});
