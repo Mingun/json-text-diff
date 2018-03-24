5d5104e5f9bdbc61d2136c20bad7e5c964cada89
Mingun <alexander_sergey@mail.ru>
2018-03-24 16:46:30 +0500

    Rework `inline` module. Add more tests

--- test/inline.js
+++ test/inline.js
@@ -1,46 +1,154 @@
 'use strict';
 
-let diff     = require('../lib/object');
-let toInline = require('../lib/inline');
+let inline = require('../lib/inline');
 
-let chai = require('chai');
+let chai   = require('chai');
 let expect = chai.expect;
 
-function inline(expected, actual) {
-  return toInline(diff(expected, actual));
-}
+describe('inline module', function() {
+  describe('converts line diff from `diff` output format to inline form', function() {
+    it('no changes translates to no changes', function() {
+      expect(inline([])).to.deep.equals([]);
+      expect(inline([
+        { value: '', removed: true },
+        { value: '', added: true },
+      ])).to.deep.equals([]);
+      expect(inline([
+        { value: '\n', removed: true },
+        { value: '', added: true },
+      ])).to.deep.equals([]);
 
-describe('inline', function() {
-  it('merge changes from changed lines', function() {
-    expect(inline('foo', 'bar')).to.deep.equals([
-      { kind: '?',
-        changes: [
-          { kind: '-', value: 'foo' },
-          { kind: '+', value: 'bar' },
-        ]
-      }
-    ]);
-  });
+      // foo
+      // bar
+      expect(inline([
+        { value: 'foo' },
+        { value: 'bar' },
+      ])).to.deep.equals([
+        { kind: ' ', value: 'foo' },
+        { kind: ' ', value: 'bar' },
+      ]);
+      // -foo
+      // +foo
+      expect(inline([
+        { value: 'foo', removed: true },
+        { value: 'foo', added: true },
+      ])).to.deep.equals([
+        { kind: ' ', value: 'foo' }
+      ]);
+    });
 
-  it('merge changes from changed multiply lines', function() {
-    let expected = 'A long time ago|in a galaxy far,|far away.... (actually not so far)'.split('|').join('\n');
-    let actual   = 'A not so long time ago|in a galaxy far,|far away....'.split('|').join('\n');
+    it('simple replace line', function() {
+      expect(inline([
+        { value: 'foo', removed: true },
+        { value: 'bar', added: true },
+      ])).to.deep.equals([
+        { kind: '?',
+          changes: [
+            { kind: '-', value: 'foo' },
+            { kind: '+', value: 'bar' },
+          ]
+        }
+      ]);
+    });
 
-    expect(inline(expected, actual)).to.deep.equals([
-      { kind: '?',
-        changes: [
-          { kind: ' ', value: 'A ' },
-          { kind: '+', value: 'not so ' },
-          { kind: ' ', value: 'long time ago' },
-        ]
-      },
-      { kind: ' ', value: 'in a galaxy far,' },
-      { kind: '?',
-        changes: [
-          { kind: ' ', value: 'far away....' },
-          { kind: '-', value: ' (actually not so far)' },
-        ]
-      },
-    ]);
+    it('additions in start of string', function() {
+      // -string
+      // +{
+      // +  string
+      // +}
+      expect(inline([
+        { value: 'string', removed: true },
+        { value: '{\n  string\n}', added: true },
+      ])).to.deep.equals([
+        { kind: '+', value: '{' },
+        { kind: '?', changes: [
+          { kind: '+', value: '  ' },
+          { kind: ' ', value: 'string' },
+        ] },
+        { kind: '+', value: '}' },
+      ]);
+
+      // -{
+      // -  string
+      // -}
+      // +string
+      expect(inline([
+        { value: '{\n  string\n}', removed: true },
+        { value: 'string', added: true },
+      ])).to.deep.equals([
+        { kind: '-', value: '{' },
+        { kind: '?', changes: [
+          { kind: '-', value: '  ' },
+          { kind: ' ', value: 'string' },
+        ] },
+        { kind: '-', value: '}' },
+      ]);
+    });
+
+    it('deletions in end of string', function() {
+      // -string 1
+      // +{
+      // +string 2
+      // +}
+      expect(inline([
+        { value: 'string 1', removed: true },
+        { value: '{\nstring 2\n}', added: true },
+      ])).to.deep.equals([
+        { kind: '+', value: '{' },
+        { kind: '?', changes: [
+          { kind: ' ', value: 'string ' },
+          { kind: '-', value: '1' },
+          { kind: '+', value: '2' },
+        ] },
+        { kind: '+', value: '}' },
+      ]);
+
+      // -{
+      // -string 1
+      // -}
+      // +string 2
+      expect(inline([
+        { value: '{\nstring 1\n}', removed: true },
+        { value: 'string 2', added: true },
+      ])).to.deep.equals([
+        { kind: '-', value: '{' },
+        { kind: '?', changes: [
+          { kind: ' ', value: 'string ' },
+          { kind: '-', value: '1' },
+          // NOTE: `diff` algorithm not so ideal, find the way fix it
+          // { kind: '+', value: '2' },
+        ] },
+        // { kind: '-', value: '}' },
+        { kind: '?', changes: [
+          { kind: '-', value: '}' },
+          { kind: '+', value: '2' },
+        ] },
+      ]);
+    });
+
+    it('changes in middle of lines', function() {
+      expect(inline([
+        { value: 'A long time ago\n', removed: true },
+        { value: 'A not so long time ago\n', added: true },
+        { value: 'in a galaxy far,\n' },
+        { value: 'far away.... (actually not so far)', removed: true },
+        { value: 'far away....', added: true },
+      ])).to.deep.equals([
+        { kind: '?',
+          changes: [
+            { kind: ' ', value: 'A ' },
+            { kind: '+', value: 'not so ' },
+            { kind: ' ', value: 'long time ago' },
+          ]
+        },
+        { kind: ' ', value: 'in a galaxy far,' },
+        { kind: '?',
+          changes: [
+            { kind: ' ', value: 'far away....' },
+            { kind: '-', value: ' (actually not so far)' },
+          ]
+        },
+      ]);
+    });
   });
 });
